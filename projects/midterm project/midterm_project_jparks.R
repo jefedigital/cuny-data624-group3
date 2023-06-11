f99d45e1630e218ffd26c1c42d06e57747baec1b
Jeff Parks <36110645+jefedigital@users.noreply.github.com>
2023-06-11 16:19:34 -0400

    midterm

--- projects/midterm project/midterm_project_jparks.R
+++ projects/midterm project/midterm_project_jparks.R
@@ -21,27 +21,32 @@ df_s01 <- df %>%
 
 df_s02 <- df %>%
   filter(category == 'S02') %>%
-  select(c(Date,Var02,Var03)) %>%
+  mutate(Date_index = row_number()) %>%
+  select(c(Date_index,Date,Var02,Var03)) %>%
   head(1622)
 
 df_s03 <- df %>%
   filter(category == 'S03') %>%
-  select(c(Date,Var05,Var07)) %>%
+  mutate(Date_index = row_number()) %>%
+  select(c(Date_index,Date,Var05,Var07)) %>%
   head(1622)
 
 df_s04 <- df %>%
   filter(category == 'S04') %>%
-  select(c(Date,Var01,Var02)) %>%
+  mutate(Date_index = row_number()) %>%
+  select(c(Date_index,Date,Var01,Var02)) %>%
   head(1622)
 
 df_s05 <- df %>%
   filter(category == 'S05') %>%
-  select(c(Date,Var02,Var03)) %>%
+  mutate(Date_index = row_number()) %>%
+  select(c(Date_index,Date,Var02,Var03)) %>%
   head(1622)
 
 df_s06 <- df %>%
   filter(category == 'S06') %>%
-  select(c(Date,Var05,Var07)) %>%
+  mutate(Date_index = row_number()) %>%
+  select(c(Date_index,Date,Var05,Var07)) %>%
   head(1622)
 
 # questions ..
@@ -50,7 +55,7 @@ df_s06 <- df %>%
 # how to handle missing data? non-ARIMA decomp functions won't handle 'internal NAs'
 
 
-## setup one analysis
+## setup single analysis
 
 my_data <- df_s01 %>%
   select(dates=Date,values=Var01) %>%
@@ -80,8 +85,9 @@ merged_obj_na <- na.locf(merged_obj)
 
 # Convert to a time series object
 # as.ts seems to drop dates and/or frequency
-my_ts <- ts(coredata(merged_obj_na)[,1], start = c(year(min(my_data$dates)), yday(min(my_data$dates))), frequency=365) 
-
+my_ts <- ts(coredata(merged_obj_na)[,1], 
+            start = c(year(min(my_data$dates)),
+                      yday(min(my_data$dates))), frequency=365) 
 
 
 ##
